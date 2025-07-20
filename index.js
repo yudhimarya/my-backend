d3bd02032571ac63adb0e97a73e80be4f936b2d7
yudhimarya <yudhisthirmaurya61@gmail.com>
2025-07-20 13:44:04 +0530

    Updated the index.js

--- index.js
+++ index.js
@@ -17,10 +17,7 @@ const app=express();
 const corsOptions = {
   origin: [
     "http://localhost:3000", 
-    "https://",
-    "https://image-generator-app-pi.vercel.app/",
-    "https://image-generator-app-ps2o.vercel.app/",
-    "https://image-generator-ddp2nuvc2-yudhisthir-mauryas-projects.vercel.app/"
+    "https://image-generator-app-blue.vercel.app/"
 
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
